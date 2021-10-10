0d056573efd9b50b99c54147db0cb0c2cea20ca1
Yan P Aung <yan.pye.aung@gmail.com>
2021-10-09 20:41:26 -0700

    Wrap button in row

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -58,13 +58,14 @@ function ProductScreen({ match }) {
               </ListGroup.Item>
 
               <ListGroup.Item>
-                <Button
-                  className="btn-block"
-                  disabled={product.countInStock === 0}
-                  type="button"
-                >
-                  Add to Cart
-                </Button>
+                <Row>
+                  <Button
+                    className="btn-block"
+                    disabled={product.countInStock === 0}
+                  >
+                    Add to Cart
+                  </Button>
+                </Row>
               </ListGroup.Item>
             </ListGroup>
           </Card>
