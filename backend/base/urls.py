9488cd261346e62cea8592778543674ceb38fb6e
Yan P Aung <yan.pye.aung@gmail.com>
2021-10-10 20:27:37 -0700

    Format code

--- backend/base/urls.py
+++ backend/base/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.get_routes, name="routes"),
-    path('products', views.get_products, name="products")
+    path("", views.get_routes, name="routes"),
+    path("products", views.get_products, name="products"),
 ]
