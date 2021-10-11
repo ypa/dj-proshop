a7fb76cbdb1bc8e80fada406dcab36c9563d4b1c
Yan P Aung <yan.pye.aung@gmail.com>
2021-10-10 20:37:02 -0700

    Add get_product view and use DRF api_view and Response

--- backend/base/urls.py
+++ backend/base/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path("", views.get_routes, name="routes"),
-    path("products", views.get_products, name="products"),
+    path("products/", views.get_products, name="products"),
+    path("products/<str:pk>", views.get_product, name="product"),
 ]
--- backend/base/views.py
+++ backend/base/views.py
@@ -1,27 +1,39 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
 from .products import products
 
 # Create your views here.
 
 
+@api_view(["GET"])
 def get_routes(request):
     routes = [
-        '/api/products/',
-        '/api/products/create',
-
-        '/api/products/upload/',
-
-        '/api/products/<id>/reviews/',
-
-        '/api/products/top/',
-        '/api/products/<id>/',
-
-        '/api/products/delete/<id>/',
-        '/api/products/<update>/<id>/',
+        "/api/products/",
+        "/api/products/create",
+        "/api/products/upload/",
+        "/api/products/<id>/reviews/",
+        "/api/products/top/",
+        "/api/products/<id>/",
+        "/api/products/delete/<id>/",
+        "/api/products/<update>/<id>/",
     ]
-    return JsonResponse(routes, safe=False)
+    return Response(routes)
 
 
+@api_view(["GET"])
 def get_products(request):
-    return JsonResponse(products, safe=False)
+    return Response(products)
+
+
+@api_view(["GET"])
+def get_product(request, pk):
+    product = None
+    for i in products:
+        if i["_id"] == pk:
+            product = i
+            break
+
+    return Response(product)
