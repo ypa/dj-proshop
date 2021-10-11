2fa01d90252c1a2ca78fd79ca923e24f1ad0cc67
Yan P Aung <yan.pye.aung@gmail.com>
2021-10-10 20:24:07 -0700

    Add views for get_routes and get_products with hard coded json

--- backend/base/views.py
+++ backend/base/views.py
@@ -1,8 +1,27 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from .products import products
 
 # Create your views here.
 
 
 def get_routes(request):
-    return JsonResponse('Hello', safe=False)
+    routes = [
+        '/api/products/',
+        '/api/products/create',
+
+        '/api/products/upload/',
+
+        '/api/products/<id>/reviews/',
+
+        '/api/products/top/',
+        '/api/products/<id>/',
+
+        '/api/products/delete/<id>/',
+        '/api/products/<update>/<id>/',
+    ]
+    return JsonResponse(routes, safe=False)
+
+
+def get_products(request):
+    return JsonResponse(products, safe=False)
