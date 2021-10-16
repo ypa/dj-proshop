2b877695f61006457c258fa8b7fc0be4e85b8f13
Yan P Aung <yan.pye.aung@gmail.com>
2021-10-15 20:39:17 -0700

    Add Error message as Message component using bootstrap Alert

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -3,6 +3,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { Row, Col } from 'react-bootstrap';
 import Product from '../components/Product';
 import Loader from '../components/Loader';
+import Message from '../components/Message';
 import { listProducts } from '../actions/productActions';
 
 function HomeScreen() {
@@ -20,7 +21,7 @@ function HomeScreen() {
       {loading ? (
         <Loader />
       ) : error ? (
-        <h3>{error}</h3>
+        <Message variant="danger">{error}</Message>
       ) : (
         <Row>
           {products.map((product) => (
