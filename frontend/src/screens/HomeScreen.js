849f5b4d63ce6639b490ad0424d37b57973e35d7
Yan P Aung <yan.pye.aung@gmail.com>
2021-10-15 20:35:24 -0700

    Render loader as bootstrap spinner component

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Row, Col } from 'react-bootstrap';
 import Product from '../components/Product';
+import Loader from '../components/Loader';
 import { listProducts } from '../actions/productActions';
 
 function HomeScreen() {
@@ -17,7 +18,7 @@ function HomeScreen() {
     <div>
       <h1>Latest Products</h1>
       {loading ? (
-        <h2>Loading...</h2>
+        <Loader />
       ) : error ? (
         <h3>{error}</h3>
       ) : (
