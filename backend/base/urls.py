686f2a2ea655caa9e154f6d2ca4e23d449345c8c
Yan P Aung <yan.pye.aung@gmail.com>
2021-10-18 20:41:48 -0700

    Configure DJRF simple jwt

--- backend/base/urls.py
+++ backend/base/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
 from . import views
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
 
 urlpatterns = [
+    path("users/login/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("", views.get_routes, name="routes"),
     path("products/", views.get_products, name="products"),
     path("products/<str:pk>", views.get_product, name="product"),
