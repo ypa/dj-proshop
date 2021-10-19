7a9dc5f71676ddda5a069180696e42178e396d88
Yan P Aung <yan.pye.aung@gmail.com>
2021-10-18 20:50:57 -0700

    Cusotmize JWT Serializer to include username and email addition to default access and refresh fields

--- backend/base/urls.py
+++ backend/base/urls.py
@@ -1,13 +1,11 @@
 from django.urls import path
 from . import views
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
 
 
 urlpatterns = [
-    path("users/login/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path(
+        "users/login/", views.MyTokenObtainPairView.as_view(), name="token_obtain_pair"
+    ),
     path("", views.get_routes, name="routes"),
     path("products/", views.get_products, name="products"),
     path("products/<str:pk>", views.get_product, name="product"),
--- backend/base/views.py
+++ backend/base/views.py
@@ -7,7 +7,22 @@ from .models import Product
 from .products import products
 from .serializers import ProductSerializer
 
-# Create your views here.
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    def validate(self, attrs):
+        data = super().validate(attrs)
+
+        data["username"] = self.user.username
+        data["email"] = self.user.email
+
+        return data
+
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
 
 
 @api_view(["GET"])
