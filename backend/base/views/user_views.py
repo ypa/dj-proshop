f80c7ec6611c612cc913f86581fe24e539d60e14
Yan P Aung <yan.pye.aung@gmail.com>
2021-10-20 20:15:49 -0700

    Refactor views and urls into own packages

--- backend/base/views/user_views.py
+++ backend/base/views/user_views.py
@@ -1,14 +1,11 @@
 from django.shortcuts import render
-from django.http import JsonResponse
 from django.contrib.auth.models import User
 from rest_framework import serializers
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
 
-from .models import Product
-from .products import products
-from .serializers import ProductSerializer, UserSerializer, UserSerializerWithToken
+from base.serializers import UserSerializer, UserSerializerWithToken
 
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
@@ -31,21 +28,6 @@ class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
 
 class MyTokenObtainPairView(TokenObtainPairView):
     serializer_class = MyTokenObtainPairSerializer
-
-
-@api_view(["GET"])
-def get_products(request):
-    products = Product.objects.all()
-    serializer = ProductSerializer(products, many=True)
-    return Response(serializer.data)
-
-
-@api_view(["GET"])
-def get_product(request, pk):
-    product = Product.objects.get(_id=pk)
-    serializer = ProductSerializer(product, many=False)
-
-    return Response(serializer.data)
 
 
 @api_view(["POST"])
