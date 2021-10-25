a339bdaaedca493a00850c5e641b988977c86564
Yan P Aung <yan.pye.aung@gmail.com>
2021-10-24 20:08:38 -0700

    Add update user profile backend view

--- backend/base/urls/user_urls.py
+++ backend/base/urls/user_urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path("login/", views.MyTokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("register/", views.register_user, name="register"),
     path("profile/", views.get_user_profile, name="users-profile"),
+    path("profile/update/", views.update_user_profile, name="users-profile-update"),
     path("", views.get_users, name="users"),
 ]
